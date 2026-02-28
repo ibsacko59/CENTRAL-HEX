8fc0a1bd5931bb892091cc609221a36ca485c235
ibsacko59 <ibsacko59@gmail.com>
2026-02-28 15:36:10 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -148,35 +148,26 @@ async function helpCommand(sock, chatId, message) {
 *Join our channel for updates:*`;
 
     try {
-        const imagePath = path.join(__dirname, '../assets/bot_image.jpg');
-        
-        const contextInfo = {
-            forwardingScore: 1,
-            isForwarded: true,
-            forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363317041132843@newsletter',
-                newsletterName: 'CENTRAL-HEX',
-                serverMessageId: -1
-            }
-        };
 
-        if (fs.existsSync(imagePath)) {
-            const imageBuffer = fs.readFileSync(imagePath);
-            await sock.sendMessage(chatId, {
-                image: imageBuffer,
-                caption: helpMessage,
-                contextInfo
-            }, { quoted: message });
-        } else {
-            await sock.sendMessage(chatId, { 
-                text: helpMessage,
-                contextInfo
-            });
+    const contextInfo = {
+        forwardingScore: 1,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363317041132843@newsletter',
+            newsletterName: 'CENTRAL-HEX',
+            serverMessageId: -1
         }
-    } catch (error) {
-        console.error('Error in help command:', error);
-        await sock.sendMessage(chatId, { text: helpMessage });
-    }
-}
+    };
 
-module.exports = helpCommand;
+    await sock.sendMessage(chatId, {
+        image: { 
+            url: 'https://i.ibb.co/RpG5TFyG/IMG-20260228-WA0167.jpg'
+        },
+        caption: helpMessage,
+        contextInfo
+    }, { quoted: message });
+
+} catch (error) {
+    console.error('Error in help command:', error);
+    await sock.sendMessage(chatId, { text: helpMessage });
+    }
